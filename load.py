ff2fedd0999eba05f0612215824b459dee47457a
Eric Chiu <ericwchiu@gmail.com>
2021-10-23 12:31:46 -0700

    can delete load by id

--- load.py
+++ load.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, Response
 from google.cloud import datastore
 
 client = datastore.Client()
@@ -70,6 +70,25 @@ def loads_get_delete(id):
         load['id'] = id
         load['self'] = request.url
         return jsonify(load), 200
+
+    # delete load 
+    elif request.method == 'DELETE':
+        load_key = client.key('loads', int(id))
+        load = client.get(key=load_key)
+
+        # boat id was not found 
+        if not load:
+            error = {"Error": "No load with this load_id exists"}
+            return jsonify(error), 404
+
+        if load['carrier']:
+            boat_key = client.key('boats', load['carrier']['id'])
+            boat = client.get(key=boat_key)
+            boat['loads'].remove({'id': load.key.id})
+            client.put(boat)
+
+        client.delete(load_key)
+        return Response(status=204)
 
     else:
         return 'Method not recognized'
